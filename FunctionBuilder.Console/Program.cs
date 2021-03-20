f3f0071439322427722e402c86afd1202834b1f9
Alexander Kirsanov <alexander.a.kirsanov@gmail.com>
2021-03-20 18:26:17 +0500

    Восстановлена работоспособность

--- FunctionBuilder.Console/Program.cs
+++ FunctionBuilder.Console/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace FunctionBuilder.Console
+﻿using System.Linq;
+
+namespace FunctionBuilder.Console
 {
     using FunctionBuilder.Logic;
     using System;
@@ -10,12 +12,7 @@
             Console.WriteLine("Hello World!");
             var func = new Function("(1/2+0.5)+2*(4-1/2)");
             Console.WriteLine(func.ToString());
-            Console.WriteLine(func.Calculate());
-
-            var op1 = new Plus();
-            var op2 = new Plus();
-            Console.WriteLine(op1 == op2);
-            Console.WriteLine(op1 == "+");
+            Console.WriteLine(func.CalculateFunctionValues().First().Value);
         }
     }
 }
--- FunctionBuilder.WPF/MainWindow.xaml.cs
+++ FunctionBuilder.WPF/MainWindow.xaml.cs
@@ -8,12 +8,6 @@ using System.Windows.Shapes;
 
 namespace FunctionBuilder.WPF
 {
-    public class FunctionValue
-    {
-        public double X { get; set; }
-        public double Y { get; set; }
-    }
-
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -47,44 +41,6 @@ namespace FunctionBuilder.WPF
                 gFunctionValues.Visibility = Visibility.Visible;
                 gFunctionValues.ItemsSource = functionValues.Select(x => new FunctionValue { X = x.Key, Y = x.Value }).ToList();
             }
-        }
-
-        private void canvas_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            Draw();
-        }
-
-        private void Draw()
-        {
-            canvas.Children.Clear();
-
-            Line xAxis = new Line();
-            double height = canvas.ActualHeight;
-            double width = canvas.ActualWidth;
-
-            xAxis.X1 = 0;
-            xAxis.Y1 = height / 2;
-            xAxis.X2 = width;
-            xAxis.Y2 = height / 2;
-            xAxis.Stroke = Brushes.Red;
-            xAxis.StrokeThickness = 2;
-
-            Polygon arrow1 = new Polygon();
-            arrow1.Points.Add(new Point(height / 2, width));
-            arrow1.Points.Add(new Point(height / 2 - 10, width - 15));
-            arrow1.Points.Add(new Point(height / 2 + 10, width - 15));
-            arrow1.Fill = Brushes.Black;
-
-            arrow1.MouseUp += arrow1_MouseUp;
-
-            canvas.Children.Add(xAxis);
-            canvas.Children.Add(arrow1);
-        }
-
-        private void arrow1_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            TestWindow testWindow = new TestWindow();
-            testWindow.Show();
         }
     }
 }
